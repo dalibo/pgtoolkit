7d560161b8d23bfe4b1d30783314bf11b56073a6
Étienne BERSAC <etienne.bersac@dalibo.com>
2018-06-19 11:51:04 +0200

    Add profile-log to measure log parser performances

--- tests/datatests.sh
+++ tests/datatests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eux
 
+is_python3() {
+        python --version |& grep -qF 3.
+}
+
 python -m pgtoolkit.hba data/pg_hba.conf
 ! (python -m pgtoolkit.hba data/pg_hba_bad.conf && exit 1)
 
@@ -10,3 +14,6 @@ python -m pgtoolkit.service data/pg_service.conf
 ! (python -m pgtoolkit.service data/pg_service_bad.conf && exit 1)
 
 python -m pgtoolkit.log '%m [%p]: [%l-1] app=%a,db=%d%q,client=%h,user=%u ' data/postgresql.log
+if is_python3 ; then
+        scripts/profile-log
+fi
