a6c44a32d07987608c63e8b2f108acb0ed60dfcb
Étienne BERSAC <etienne.bersac@dalibo.com>
2018-08-06 15:47:51 +0200

    Version 0.0.1a0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 metadatas = dict(
     name='pgtoolkit',
     description='Manage Postgres cluster files from Python',
-    version='0.0.0',
+    version='0.0.1a0',
     author='Dalibo',
     author_email='contact@dalibo.com',
     license='PostgreSQL',
