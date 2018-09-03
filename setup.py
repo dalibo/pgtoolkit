8d804ee300ab994d02659859bedde59ffa329707
Étienne BERSAC <etienne.bersac@dalibo.com>
2018-09-03 15:40:14 +0200

    Increment version to development version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 metadatas = dict(
     name='pgtoolkit',
     description='Manage Postgres cluster files from Python',
-    version='0.0.1a1',
+    version='0.2.0.dev0',
     author='Dalibo',
     author_email='contact@dalibo.com',
     url='https://github.com/dalibo/pgtoolkit',
