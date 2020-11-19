cfec3d56829fc71bc46d18dff5f4cb2042031e86
Denis Laxalde <denis.laxalde@dalibo.com>
2020-11-19 11:53:18 +0100

    Version 0.10.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 metadatas = dict(
     name='pgtoolkit',
     description='Postgres Support from Python',
-    version='0.10.0',
+    version='0.10.1',
     author='Dalibo',
     author_email='contact@dalibo.com',
     url='https://github.com/dalibo/pgtoolkit',
