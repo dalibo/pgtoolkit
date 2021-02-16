e3c8c049c29ed07b9bb67a43b3903bbdfb9b5517
Denis Laxalde <denis.laxalde@dalibo.com>
2021-02-16 14:11:05 +0100

    Extend metadata

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 from setuptools import setup
 
 
+with open("README.rst", "r", encoding="utf-8") as fo:
+    long_description = fo.read()
+
 metadatas = dict(
     name="pgtoolkit",
-    description="Postgres Support from Python",
     version="0.13.0",
+    description="PostgreSQL Support from Python",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     author="Dalibo",
     author_email="contact@dalibo.com",
     url="https://github.com/dalibo/pgtoolkit",
     license="PostgreSQL",
+    keywords="postgresql postgresql.conf pg_hba pgpass pg_service.conf",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: PostgreSQL License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Database",
+    ],
 )
 
 
